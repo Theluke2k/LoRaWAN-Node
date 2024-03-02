8be8b85c24abab6825245d5912c107effdc5c5fe
Theluke2k <46893659+Theluke2k@users.noreply.github.com>
2024-03-02 17:47:35 +0100

    Deleted some regional files not needed

--- tdr-board-v4-LORAWAN/src/main.c
+++ tdr-board-v4-LORAWAN/src/main.c
@@ -57,7 +57,7 @@ uint8_t tester = 0;
 /*!
  * Defines the application data transmission duty cycle. 10s, value in [ms].
  */
-#define APP_TX_DUTYCYCLE                            120000 // minimum 4
+#define APP_TX_DUTYCYCLE                            8000 // minimum 4
 
 /*!
  * Defines a random delay for application data transmission duty cycle. 1s,
@@ -77,7 +77,7 @@ uint8_t tester = 0;
  *
  * \remark Please note that LORAWAN_DEFAULT_DATARATE is used only when ADR is disabled
  */
-#define LORAWAN_DEFAULT_DATARATE                    DR_3
+#define LORAWAN_DEFAULT_DATARATE                    DR_0
 
 /*!
  * LoRaWAN confirmed messages
@@ -94,7 +94,7 @@ uint8_t tester = 0;
  *
  * \remark Please note that ETSI mandates duty cycled transmissions. Use only for test purposes
  */
-#define LORAWAN_DUTYCYCLE_ON                        true //DEBUG
+#define LORAWAN_DUTYCYCLE_ON                        true //DEBUG (default = true)
 
 /*!
  * LoRaWAN application port (Hvad skal denne sættes til?)
